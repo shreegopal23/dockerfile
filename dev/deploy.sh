999655f286319b2052bc610a62720ed95612d939
shreegopal_dadhich <shreegopaldadhich.lw@gmail.com>
2025-05-23 22:24:21 +0530

    second

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,4 +1,4 @@
-docker pull logicwind/appsonair-graphql-server-v2:dev
+docker pull image name :dev
 docker-compose -f docker-compose-server.yml down
 docker-compose -f docker-compose-server.yml up -d
 docker image prune -f
